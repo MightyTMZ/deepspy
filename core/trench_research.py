eaaebedc54ab9279dba841bf91e80ecc25a407f0
Tom Zhang <tommyawesome08@gmail.com>
2026-09-12 17:13:11 -0400

    trench research

---
+++ core/trench_research.py
@@ -0,0 +1,7 @@
+"""
+'Deep' research only seems to touch the surface of the trench. 
+It is like snorkel diving in a deep trench, where you can see the surface but not the depths below.
+
+DeepSpy is here to introduce a new way of doing research on your competitors, where we can go deeper into the trench and explore the depths below.
+"""
+
