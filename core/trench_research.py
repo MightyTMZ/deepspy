8b6f4e9b577394dfe4e6dd22712356f760181413
Tom Zhang <tommyawesome08@gmail.com>
2026-09-12 22:37:53 -0400

    delete old architecture

--- core/trench_research.py
+++
@@ -1,7 +0,0 @@
-"""
-'Deep' research only seems to touch the surface of the trench. 
-It is like snorkel diving in a deep trench, where you can see the surface but not the depths below.
-
-DeepSpy is here to introduce a new way of doing research on your competitors, where we can go deeper into the trench and explore the depths below.
-"""
-
